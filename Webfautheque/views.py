051d7ec265111e1213c7fd55602179b602a6404a
rcarl <rcarlier.romain@gmail.com>
2022-05-24 17:01:55 +0200

    réajustement css ajout/update

--- Webfautheque/views.py
+++ Webfautheque/views.py
@@ -34,8 +34,6 @@ from .models import Classe, Defaut, Experience, Groupe, Sous_groupe
 """
 
 # insertion des groupes B200 à G200 dans la base de données
-
-
 def insert_groupes():
     tab = ['B200', 'B300', 'C100', 'C200', 'C300', 'C400', 'D100',
            'D200', 'E100', 'E200', 'F100', 'F200', 'G100', 'G200']
@@ -43,7 +41,6 @@ def insert_groupes():
         tab1 = i[0]
         cla_id = Classe.objects.filter(classe_idperso=tab1).values()[0]["id"]
         Groupe.objects.create(groupe_idperso=i, groupe_nom=i, classe_id=cla_id)
-
 
 # insertion des sous_groupes de B220 à G260
 def insert_sous_groupe():
@@ -122,7 +119,6 @@ def insert_sous_groupe():
     Sous_groupe.objects.create(
         sous_groupe_idperso='G260', sous_groupe_nom='G260', groupe_id=18)
 
-
 # modification de la valeur 'sous_groupes' de la table défaut
 # fonction permettant de classer tout les defauts dans le bon groupe
 def update_defaut():
@@ -182,6 +178,9 @@ def afficherTiret(modelObject, lib_object):
 
     tab.append(first_string)
     return tab
+
+
+
 
 
 """
@@ -296,50 +295,53 @@ def page_consultation_experience(request, defaut_idperso, experience_id):
     except :
         return redirect('page_choix_experience', defaut_idperso)
 
+
 @permission_required('Webfautheque.add_experience')
 def page_ajout_experience(request, defaut_idperso = ''):
     # ajout d'un formulaire Expérience
-    list_option = None
-    form = ExperienceForm()
+    try:
+        form = ExperienceForm()
 
-    #Si Envoie formulaire :  
-    if request.method == 'POST':
-        form = ExperienceForm(request.POST, request.FILES or None)
-        if form.is_valid():
+        #Si Envoie formulaire :  
+        if request.method == 'POST':
+            form = ExperienceForm(request.POST, request.FILES or None)
+            if form.is_valid():
 
-            # date non naive à la date du jour
-            naive_datetime = datetime.datetime.now()
-            naive_datetime.tzinfo  # None
-            settings.TIME_ZONE  # 'UTC'
+                # date non naive à la date du jour
+                naive_datetime = datetime.datetime.now()
+                naive_datetime.tzinfo  # None
+                settings.TIME_ZONE  # 'UTC'
 
-            experience = form.save(commit=False)
-            
+                experience = form.save(commit=False)
+                
 
-            experience.defaut_id = request.POST.get('defaut')
-            experience.experience_pub_date = naive_datetime
-            experience.save()
+                experience.defaut_id = request.POST.get('defaut')
+                experience.experience_pub_date = naive_datetime
+                experience.save()
 
-            return redirect('experience_list')
+                return redirect('experience_list')
 
-    # Affichage de la page quand ce n'est pas l'envoie du formulaire 
-    else:
-        form = ExperienceForm(initial={ 'experience_auteur': request.user, 'experience_pub_date': datetime.datetime.now()})
-        list_option = Defaut.objects.all()
-        page = request.GET.get('page', 1)
-
-        if(page == 'ajout_list'):
-            defaut_nom = Defaut.objects.all()
-            return render(request, 'Webfautheque/experience_ajout.html', {'experience_form': form, 'liste_defaut': defaut_nom})
+        # Affichage de la page quand ce n'est pas l'envoie du formulaire 
         else:
-            form.fields['defaut'].queryset = Defaut.objects.filter(defaut_idperso=defaut_idperso)
-            form.fields['defaut'].label = "Défaut" 
-            form.fields['defaut'].empty_label = None
+            form = ExperienceForm(initial={ 'experience_auteur': request.user, 'experience_pub_date': datetime.datetime.now()})
+            page = request.GET.get('page', 1)
 
-            liste_defaut = Defaut.objects.all()
-            defaut_nom = Defaut.objects.get(defaut_idperso=defaut_idperso).defaut_nom
-            defaut_id = Defaut.objects.get(defaut_idperso=defaut_idperso).id
- 
-            return render(request, 'Webfautheque/experience_ajout.html', {'experience_form': form, 'defaut': defaut_idperso, 'nom_defaut': defaut_nom, 'defaut_id': defaut_id, 'liste_defaut': liste_defaut})
+            if(page == 'ajout_list'):
+                defaut_nom = Defaut.objects.all()
+                return render(request, 'Webfautheque/experience_ajout.html', {'experience_form': form, 'liste_defaut': defaut_nom})
+            else:
+                form.fields['defaut'].queryset = Defaut.objects.filter(defaut_idperso=defaut_idperso)
+                form.fields['defaut'].label = "Défaut" 
+                form.fields['defaut'].empty_label = None
+
+                liste_defaut = Defaut.objects.all()
+                defaut_nom = Defaut.objects.get(defaut_idperso=defaut_idperso).defaut_nom
+                defaut_id = Defaut.objects.get(defaut_idperso=defaut_idperso).id
+    
+                return render(request, 'Webfautheque/experience_ajout.html', {'experience_form': form, 'defaut': defaut_idperso, 'nom_defaut': defaut_nom, 'defaut_id': defaut_id, 'liste_defaut': liste_defaut})
+    except :
+        return redirect('page_choix_experience', defaut_idperso)
+
 
 @permission_required('Webfautheque.change_experience', login_url='/login/')
 def page_update_experience(request, id, experience_id):
@@ -358,12 +360,15 @@ def page_update_experience(request, id, experience_id):
             return redirect('experience_list')
         else:
             nom_exp = Experience.objects.get(id=experience_id).experience_nom_article
-
+            liste_defaut = Defaut.objects.all()
+            defaut_nom = Defaut.objects.get(defaut_idperso=id).defaut_nom
+            defaut_id = Defaut.objects.get(defaut_idperso=id).id
             id_defaut = Defaut.objects.get(id=Experience.objects.get(id=experience_id).defaut_id).defaut_idperso
-            return render(request, 'Webfautheque/experience_update.html', {'experience_form': form, 'experience_id': experience_id, 'nom_exp': nom_exp, 'id_defaut' : id_defaut})
+            return render(request, 'Webfautheque/experience_update.html', {'experience_form': form, 'experience_id': experience_id, 'nom_exp': nom_exp, 'id_defaut' : id_defaut, 'liste_defaut': liste_defaut, 'nom_defaut' : defaut_nom, 'defaut_id' : defaut_id})
 
     except :
         return redirect('experience_list')
+
 
 def experience_list(request):
     experiences = Experience.objects.all().order_by('-experience_pub_date')
@@ -391,6 +396,7 @@ def page_delete_experience(request, id, experience_id):
     except:
         return redirect('experience_list')
 
+
 def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -403,6 +409,7 @@ def login_user(request):
             return render(request, 'Webfautheque/login.html', {'error_message': 'Identifiants incorrects'})
     else:
         return render(request, 'Webfautheque/login.html')
+
 
 @login_required(login_url='/')
 def logout_user(request):
@@ -457,6 +464,7 @@ def experienceAuteurDefaut(request, defaut_idperso):
             for experience in data:
                 experience['defaut_nom'] = Defaut.objects.get(id=experience['defaut_id']).defaut_idperso
             return JsonResponse(list(data), safe=False,)
+
 
 # ajout d'un jeu de données
 # def fakerExperience(request):
